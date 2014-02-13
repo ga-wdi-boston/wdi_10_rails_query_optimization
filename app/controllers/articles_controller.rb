b6ac80e4d197e6c12722de0533cc2db85d57d634
Alex Grant <grantovich@users.noreply.github.com>
2014-02-13 10:00:27 -0500

    Visitors must sign in to create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @articles = Article.order(created_at: :desc)
   end
--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -1,28 +1,39 @@
 require 'rails_helper'
 
 feature 'User creates an article' do
-  background do
-    @user = create(:user)
-    sign_in_as(@user)
-    click_link 'Submit article'
+  scenario 'unsuccessfully when not signed in' do
+    visit root_path
+    expect(page).to_not have_link 'Submit article'
+
+    visit new_article_path
+    expect(page).to_not have_button 'Create Article'
+    expect(page).to have_content 'You need to sign in or sign up'
   end
 
-  scenario 'successfully' do
-    fill_in 'Title', with: 'Better Specs!'
-    fill_in 'URL', with: 'http://betterspecs.org'
-    click_button 'Create Article'
+  context 'when signed in' do
+    background do
+      @user = create(:user)
+      sign_in_as(@user)
+      click_link 'Submit article'
+    end
 
-    expect(page).to have_content 'Article submitted!'
-    expect(page).to have_link 'Better Specs!', href: 'http://betterspecs.org'
-    expect(page).to have_content "by #{@user.email} less than a minute ago"
-  end
+    scenario 'successfully' do
+      fill_in 'Title', with: 'Better Specs!'
+      fill_in 'URL', with: 'http://betterspecs.org'
+      click_button 'Create Article'
 
-  scenario 'unsuccessfully with an invalid URL' do
-    fill_in 'URL', with: 'ftp://haxxored.info'
-    click_button 'Create Article'
+      expect(page).to have_content 'Article submitted!'
+      expect(page).to have_link 'Better Specs!', href: 'http://betterspecs.org'
+      expect(page).to have_content "by #{@user.email} less than a minute ago"
+    end
 
-    expect(page).to_not have_content 'Article submitted!'
-    expect(page).to have_content "Title can't be blank"
-    expect(page).to have_content "Url is not a valid URL"
+    scenario 'unsuccessfully with an invalid URL' do
+      fill_in 'URL', with: 'ftp://haxxored.info'
+      click_button 'Create Article'
+
+      expect(page).to_not have_content 'Article submitted!'
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Url is not a valid URL"
+    end
   end
 end
