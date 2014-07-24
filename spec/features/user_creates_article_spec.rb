sassynerd13/haxor-news

Users can submit new articles

spec/features/user_creates_article_spec.rb
@@ -1,0 +1,16 @@
+require 'spec_helper'
+
+feature 'User creates an article' do
+  scenario 'successfully' do
+    user = create(:user)
+    sign_in_as(user)
+
+    click_link 'Submit article'
+    fill_in 'Title', with: 'Better Specs!'
+    fill_in 'URL', with: 'http://betterspecs.org'
+    click_button 'Create Article'
+
+    expect(page).to have_link 'Better Specs!', href: 'http://betterspecs.org'
+    expect(page).to have_content "by #{user.email} less than a minute ago"
+  end
+end

ga-wdi-boston/wdi_10_rails_query_optimization

spec/features/user_creates_article_spec.rb
@@ -1,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User creates an article' do
+  scenario 'successfully' do
+    user = create(:user)
+    sign_in_as(user)
+
+    click_link 'Submit article'
+    fill_in 'Title', with: 'Better Specs!'
+    fill_in 'URL', with: 'http://betterspecs.org'
+    click_button 'Create Article'
+
+    expect(page).to have_link 'Better Specs!', href: 'http://betterspecs.org'
+    expect(page).to have_content "by #{user.email} less than a minute ago"
+  end
+end