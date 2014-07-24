sassynerd13/haxor-news

Users can upvote articles

spec/features/user_votes_article_spec.rb
@@ -1,0 +1,14 @@
+require 'spec_helper'
+
+feature 'User votes an article' do
+  scenario 'up' do
+    target_article = create_list(:article, 3).second
+    sign_in_as(create(:user))
+
+    within('div', text: target_article.title) { click_link 'Upvote' }
+
+    within('div', text: target_article.title) do
+      expect(page).to have_content '1 point'
+    end
+  end
+end

ga-wdi-boston/wdi_10_rails_query_optimization

spec/features/user_votes_article_spec.rb
@@ -1,0 +1,14 @@
+require 'rails_helper'
+
+feature 'User votes an article' do
+  scenario 'up' do
+    target_article = create_list(:article, 3).second
+    sign_in_as(create(:user))
+
+    within('div', text: target_article.title) { click_link 'Upvote' }
+
+    within('div', text: target_article.title) do
+      expect(page).to have_content '1 point'
+    end
+  end
+end